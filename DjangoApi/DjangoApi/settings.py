dad4a7513e01efa2831fc4bc734b1e9713660709
Полина Тригуб PD-87 <doc21@inbox.ru>
2025-03-24 01:55:08 +0300

    хоспадебоже, два часа ночи, а я исправляю эту хуету (:

--- DjangoApi/DjangoApi/settings.py
+++ DjangoApi/DjangoApi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9t#=@+ftt79u@c!w)v2$fzoif2yf9_&gi*y_k4@$@(j$qp3%5)'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
