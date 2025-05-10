4811472647bd87a2492f445694893a74cab25de3
Полина Тригуб PD-87 <doc21@inbox.ru>
2025-05-10 15:46:00 +0300

    corrected dockerfile, readme and urls

--- DjangoApi/DjangoApi/urls.py
+++ DjangoApi/DjangoApi/urls.py
@@ -3,28 +3,13 @@ URL-конфигурация для DjangoApi.
 
 Схема URL-адресов:
 admin/ - админка
-swagger/ - документация API
-redoc/ - документация API
-api/ - API
-api/token/ - токены
-api/token/refresh/ - обновление токена
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from .yasg import schema_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('', include('app.urls')),
 ]
--- DjangoApi/app/urls.py
+++ DjangoApi/app/urls.py
@@ -3,10 +3,18 @@ URLs для DjangoApi.
 
 Схема URL-адресов:
 api/ - API
+api/token/ - токены
+api/token/refresh/ - обновление токена
+
+swagger/ - документация API
+redoc/ - документация API
 """
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from DjangoApi.yasg import schema_view
 from .api import TaskSet, UserSet
 
 router = DefaultRouter()
@@ -15,4 +23,10 @@ router.register(r'users', UserSet, basename='user')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 ]
